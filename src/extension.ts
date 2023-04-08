b2337160ac3f70b9e5e6dcf5ea9c86ec38e52d09
Valentin <valentin.quelquejay@consensys.net>
2023-04-08 15:25:36 +0200

    * add support for .gitignore pattern * switch from file-based marked file storage to workspace memento * add an option to export marked files to disk * various fixes * remove compilation artifacts * add testing infrastructure

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,28 @@
 import * as vscode from "vscode";
 import { DecorationProvider } from "./fileDecorationProvider";
 
-var provider: DecorationProvider;
+let provider: DecorationProvider;
+let outputChannel: vscode.OutputChannel;
+let storage: vscode.Memento;
 
 // This method is called when the extension is activated
 export function activate(context: vscode.ExtensionContext) {
+  //create WS storage
+
+  storage = context.workspaceState;
+  if (!storage) {
+    vscode.window.showInformationMessage(
+      "Please create a workspace to save the marked files"
+    );
+  }
+  //initialize output channel
+  outputChannel = vscode.window.createOutputChannel("Mark Files");
+
   //register the decoration provider
   provider = new DecorationProvider();
-  let disposable = vscode.window.registerFileDecorationProvider(provider);
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerFileDecorationProvider(provider)
+  );
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
@@ -16,16 +30,17 @@ export function activate(context: vscode.ExtensionContext) {
       async (contextUri: vscode.Uri) => {
         const uri = vscode.window.activeTextEditor?.document.uri;
         if (uri) {
-          const stat = await vscode.workspace.fs.stat(uri);
-          if (stat.type !== vscode.FileType.File) {
-            return;
-          } //can't mark directory
-          if (provider.markedFiles.has(uri.fsPath)) {
-            provider.update([], [uri.fsPath]);
-          } else {
-            provider.update([uri.fsPath], []);
-          }
+          provider.markOrUnmarkFiles([uri]);
         }
+      }
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "markfiles.writeMarkedFilesToDisk",
+      async () => {
+        await provider.exportMarkedFilesToFile();
       }
     )
   );
@@ -34,19 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand(
       "markfiles.markUnmarkSelectedFile",
       async (clickedFile: vscode.Uri, selectedFiles: vscode.Uri[]) => {
-        for (const uri of selectedFiles) {
-          if (uri) {
-            const stat = await vscode.workspace.fs.stat(uri);
-            if (stat.type !== vscode.FileType.File) {
-              continue;
-            } //can't mark directory
-            if (provider.markedFiles.has(uri.fsPath)) {
-              provider.update([], [uri.fsPath]);
-            } else {
-              provider.update([uri.fsPath], []);
-            }
-          }
-        }
+        provider.markOrUnmarkFiles(selectedFiles);
       }
     )
   );
@@ -63,14 +66,39 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
-  //listen to configuration changes
-  vscode.workspace.onDidChangeConfiguration((e) => {
-    if (e.affectsConfiguration("markfiles")) {
-      provider.configChanged();
-    }
-  });
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    //listen to configuration changes
+    vscode.workspace.onDidChangeConfiguration((e) => {
+      if (e.affectsConfiguration("markfiles")) {
+        provider.configChanged();
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.workspace.onDidRenameFiles(async (rename) => {
+      if (rename.files.length === 0) {
+        return;
+      }
+
+      for (const file of rename.files) {
+        provider.handleFileRename(file.oldUri, file.newUri);
+      }
+    })
+  );
 }
+
+//print to the output channel
+export const printChannelOutput = (content: string, reveal = false): void => {
+  outputChannel.appendLine(content);
+  if (reveal) {
+    outputChannel.show(true);
+  }
+};
+
+export const getStorage = () => {
+  return storage;
+};
 
 // This method is called when the extension is deactivated
 export function deactivate() {}
